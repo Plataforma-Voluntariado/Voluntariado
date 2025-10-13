73b28d8dfbda3befc4ca86e8bd241eecaef77a91
Davidshtp <davidtrujillo2207@gmail.com>
2025-10-13 16:24:45 -0500

    44-Proteccion de rutas privadas en el login y verificacion

--- backend/src/usuario/usuario.controller.ts
+++ backend/src/usuario/usuario.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus, UseGuards } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { solicitudVerificacionCorreoDto } from './dto/solicitud-verificacion-correo.dto';
 import { VerificacionCorreoDto } from './dto/validar-codigo-verificacion.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 
 @Controller('usuarios')
@@ -21,11 +22,13 @@ export class UsuarioController {
   }
 
   @Post('solicitud-verificacion-correo')
+  @UseGuards(JwtAuthGuard)
   async solicitarRecuperacion(@Body() solicitudVerificacionCorreoDto:solicitudVerificacionCorreoDto) {
     return this.usuarioService.solicitarVerificacionCorreo(solicitudVerificacionCorreoDto);
   }
 
   @Post('verificacion-correo')
+  @UseGuards(JwtAuthGuard)
   async verificacionCorreo(@Body() verificacionCorreodto: VerificacionCorreoDto) {
     return this.usuarioService.validarCodigoVerificacionCorreo(verificacionCorreodto);
   }
