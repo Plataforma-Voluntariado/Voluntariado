5c6247d6106ecfbb771edc0e1b0ed0c91650239b
FelipeFajardo1 <felipealekand@gmail.com>
2025-10-26 15:27:38 -0500

    Migracion Categorias

--- backend/nest-app/src/app.module.ts
+++ backend/nest-app/src/app.module.ts
@@ -37,7 +37,7 @@ import { GatewaysModule } from './gateways/gateways.module';
         synchronize: false,
         logging: false,
         migrations: [__dirname + '/database/migrations/*{.ts,.js}'],
-        migrationsRun: true, 
+        migrationsRun: false, 
       }),
       inject: [ConfigService],
     }),
