400c3b5926d26ace1173a2dfba0a301a269337a7
Neonwaac <jomaparo3103@gmail.com>
2025-10-27 14:10:07 -0500

    Añadir funcionalidad de actualizar y eliminar foto desde el frontend

--- frontend/src/components/Profile/ProfileArchive/ProfileArchive.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./ProfileArchive.css";
-function ProfileArchive({ user }) {
-    return(
-        <div className="profile-archive"></div>
-    );
-}
--- frontend/src/services/auth/UserManagementService.jsx
+++ frontend/src/services/auth/UserManagementService.jsx
@@ -14,24 +14,28 @@ export const GetUsersByRole = async (rol) => {
 };
 
 export const GetUserByVerificationId = async (idVerificacion) => {
-    try {
-        const response = await api.get("verificacion/archivos-pendientes/"+idVerificacion);
-        return response.data;
-    }catch (error) {
-        console.error("Error obteniendo el usuario por ID de verificación.", error);
-        return error;
-      }
-}
+  try {
+    const response = await api.get(
+      "verificacion/archivos-pendientes/" + idVerificacion
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error obteniendo el usuario por ID de verificación.", error);
+    return error;
+  }
+};
 
-export const GetUserFile = async (idVerificacionArchivo) =>{
-    try{
-        const response = await api.get("verificacion-archivo/ver/"+idVerificacionArchivo)
-        return response.data;
-    }catch(error){
-        console.error("Error obteniendo el archivo PDF del usuario.", error);
-        return error;
-    }
-}
+export const GetUserFile = async (idVerificacionArchivo) => {
+  try {
+    const response = await api.get(
+      "verificacion-archivo/ver/" + idVerificacionArchivo
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error obteniendo el archivo PDF del usuario.", error);
+    return error;
+  }
+};
 
 export const RejectUserFile = async (idVerificacionArchivo, observacion) => {
   try {
@@ -49,27 +53,28 @@ export const RejectUserFile = async (idVerificacionArchivo, observacion) => {
   }
 };
 
-
-export const AcceptUserFile = async (idVerificacionArchivo) =>{
-    try{
-        const response = await api.post("verificacion-archivo/aceptar/"+idVerificacionArchivo);
-        console.log("verificacion-archivo/aceptar/"+idVerificacionArchivo)
-        return response;
-    }catch(error){
-        console.error("Error aceptando el archivo del usuario.", error);
-        return error;
-    }
-}
+export const AcceptUserFile = async (idVerificacionArchivo) => {
+  try {
+    const response = await api.post(
+      "verificacion-archivo/aceptar/" + idVerificacionArchivo
+    );
+    console.log("verificacion-archivo/aceptar/" + idVerificacionArchivo);
+    return response;
+  } catch (error) {
+    console.error("Error aceptando el archivo del usuario.", error);
+    return error;
+  }
+};
 
 export const GetUserFilesArray = async () => {
-    try{
-        const response = await api.get("verificacion/mis-archivos");
-        return response.data.archivos;
-    }catch(error){
-        console.error("Error obteniendo los archivos del usuario.", error);
-        return error;
-    }
-}
+  try {
+    const response = await api.get("verificacion/mis-archivos");
+    return response.data.archivos;
+  } catch (error) {
+    console.error("Error obteniendo los archivos del usuario.", error);
+    return error;
+  }
+};
 
 export const UploadUserFile = async (fileData, fileType) => {
   try {
@@ -90,3 +95,29 @@ export const UploadUserFile = async (fileData, fileType) => {
     return error || { error: "Error al subir archivo" };
   }
 };
+
+export const UploadProfilePhoto = async (photoFile) => {
+  try {
+    const formData = new FormData();
+    formData.append("imagen", photoFile);
+    const response = await api.post("/usuarios/imagen-perfil", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return response;
+  } catch (error) {
+    console.error("Error subiendo la foto de perfil:", error);
+    return error || { error: "Error al subir foto de perfil" };
+  }
+};
+
+export const DeleteProfilePhoto = async () => {
+  try{
+    const response = await api.delete("/usuarios/imagen-perfil");
+    return response;
+  } catch (error) {
+    console.error("Error eliminando la foto de perfil:", error);
+    return error || { error: "Error al eliminar foto de perfil" };
+  }
+}
