1e901e846b7c195af214a1f1f0dd5544441e7732
FelipeFajardo1 <felipealekand@gmail.com>
2025-11-08 18:42:36 -0500

    Bug Fixed 1.1

---
+++ backend/nest-app/copy-db.js
@@ -0,0 +1,29 @@
+// copy-db.js
+import fs from "fs";
+import path from "path";
+
+const src = path.join("src", "database", "colombia");
+const dest = path.join("dist", "database", "colombia");
+
+if (fs.existsSync(src)) {
+  fs.mkdirSync(dest, { recursive: true });
+
+  const copyRecursive = (srcDir, destDir) => {
+    for (const file of fs.readdirSync(srcDir)) {
+      const srcPath = path.join(srcDir, file);
+      const destPath = path.join(destDir, file);
+
+      if (fs.lstatSync(srcPath).isDirectory()) {
+        fs.mkdirSync(destPath, { recursive: true });
+        copyRecursive(srcPath, destPath);
+      } else {
+        fs.copyFileSync(srcPath, destPath);
+      }
+    }
+  };
+
+  copyRecursive(src, dest);
+  console.log("✅ Carpeta 'src/database/colombia' copiada correctamente.");
+} else {
+  console.log("⚠️ Carpeta 'src/database/colombia' no encontrada, omitiendo copia.");
+}
